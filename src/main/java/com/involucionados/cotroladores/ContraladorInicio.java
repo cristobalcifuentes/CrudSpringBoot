761a59b6ab1c1c6b3b0a63bfcb9008bf63aaad20
cristobal <c.cifuentes1990@gmail.com>
2020-07-23 18:28:46 -0400

    iniciamos crud spring boot

---
+++ src/main/java/com/involucionados/cotroladores/ContraladorInicio.java
@@ -0,0 +1,21 @@
+package com.involucionados.cotroladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ContraladorInicio {
+	
+	@GetMapping(value={"/home", "", "/", "/index"})
+	public String inicio() {
+		
+		
+		return "index";
+		
+	}
+	
+	
+	
+	
+}
